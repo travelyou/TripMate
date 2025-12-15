3c46e451736d9d10dcec962e46889197048f6b25
chyt93620 <235834389+chyt93620@users.noreply.github.com>
2025-12-15 22:11:58 +0800

    切了一點step1的版

--- src/views/CheckoutLayout.vue
+++ src/views/CheckoutLayout.vue
@@ -3,8 +3,5 @@ import CheckoutProgress from '../components/checkout/CheckoutProgress.vue'
 </script>
 <template>
   <CheckoutProgress :current-step="1" />
-  <div>
-    <h1>結帳流程</h1>
-  </div>
   <router-view />
 </template>
