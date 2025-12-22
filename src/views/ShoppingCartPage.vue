a947d7155128a2578c35d7b75d6ca81c904fbd80
chyt93620 <chyt93620@gmail.com>
2025-12-22 14:52:53 +0800

    style:購物車RWD完成

--- src/views/ShoppingCartPage.vue
+++ src/views/ShoppingCartPage.vue
@@ -82,55 +82,42 @@ function goToCheckout() {
   }
 }
 
-//檢測是否登入才能結帳
-// function goToCheckout() {
-//   if (userStore.isLoggedIn) {
-//     if (selectedTour.value) {
-//       checkoutStore.selectedTour = selectedTour.value
-//       router.push('/checkout/step1')
-//     } else {
-//       // 沒有選中項目
-//       alert('請先選擇一個要結帳的行程！')
-//     }
-//   } else {
-//     // 沒有選中項目
-//     alert('請先登入！')
-//   }
-// }
-
 function goToFeatured() {
   router.push('/featured-itinerary')
 }
 </script>
 
 <template>
-  <section class="max-w-5xl mx-auto mt-10 mr-20">
-    <h1 class="text-3xl font-bold">購物車</h1>
-    <div class="flex gap-5">
-      <div v-show="!isCartEmpty" class="p-5 rounded-md">
-        <ul class="grid gap-5 min-w-130">
+  <section class="max-w-5xl mx-auto mt-5 p-5 md:mr-0 lg:mr-20 xl:mr-20">
+    <h1 class="text-3xl font-bold ml-8 mb-5">購物車</h1>
+
+    <div class="flex flex-col gap-5 lg:flex-row">
+      <div v-show="!isCartEmpty" class="rounded-md">
+        <ul class="grid gap-5">
           <li
             v-for="tour in tourGroups"
             :key="tour.id"
-            class="p-5 border border-gray-200 rounded-xl bg-white hover:bg-gray-100"
+            class="p-5 border border-gray-200 rounded-xl bg-white hover:bg-gray-100 min-w-[400px] lg:min-w-[500px] md:"
           >
             <div class="flex justify-between gap-10">
               <div class="flex gap-5">
                 <input v-model="selectedTourId" type="radio" name="tour" :value="tour.id" />
-                <img
-                  v-if="tour.image"
-                  :src="tour.image"
-                  alt="旅遊圖片"
-                  class="w-32 h-24 rounded-lg overflow-hidden flex-shrink-0"
-                />
-                <div class="flex flex-col justify-between">
-                  <div>
-                    <h1 class="font-bold">{{ tour.title }}</h1>
-                    <p class="text-sm text-gray-500">{{ tour.description }}</p>
-                  </div>
-                  <div class="flex gap-5 mt-5">
-                    <p class="text-sm text-gray-500">{{ tour.date }}</p>
-                    <p class="text-sm text-gray-500">{{ tour.duration }}</p>
+                <div class="flex flex-col gap-5 sm:flex-row">
+                  <img
+                    v-if="tour.image"
+                    :src="tour.image"
+                    alt="旅遊圖片"
+                    class="w-32 h-24 rounded-lg overflow-hidden flex-shrink-0"
+                  />
+                  <div class="flex flex-col justify-between">
+                    <div>
+                      <h1 class="font-bold">{{ tour.title }}</h1>
+                      <p class="text-sm text-gray-500">{{ tour.description }}</p>
+                    </div>
+                    <div class="flex gap-5 mt-5">
+                      <p class="text-sm text-gray-500">{{ tour.date }}</p>
+                      <p class="text-sm text-gray-500">{{ tour.duration }}</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -161,6 +148,7 @@ function goToFeatured() {
           </li>
         </ul>
       </div>
+
       <div v-show="isCartEmpty" class="max-w-5xl mx-auto mt-10 text-center">
         <div class="mb-5">
           <h1 class="text-xl">購物車是空的</h1>
@@ -173,36 +161,36 @@ function goToFeatured() {
           前往精選行程
         </router-link>
       </div>
-      <div
-        v-show="!isCartEmpty"
-        class="p-5 bg-white rounded-md flex flex-col justify-between min-w-60"
-      >
-        <div>
-          <h1>結算資訊</h1>
-          <p class="text-gray-400 text-sm mb-5">請選擇要結帳的行程</p>
-        </div>
-        <div>
-          <div class="space-y-3 mb-6">
-            <div class="flex justify-between text-gray-700">
-              <span>人數</span>
-              <span>{{ selectedTour ? selectedTour.persons : 0 }}</span>
-            </div>
-            <div class="flex justify-between text-gray-700">
-              <span>金額 </span>
-              <span>NT$ {{ selectedTour ? selectedTour.price : 0 }} </span>
-            </div>
-          </div>
-          <div class="border-t border-gray-300 pt-3 pb-3">
-            <div class="flex justify-between text-gray-700">
-              <span>小記</span>
-              <span>NT$ {{ totalPrice }}</span>
-            </div>
-          </div>
-        </div>
 
-        <div class="flex flex-col gap-5">
-          <MainButton @click="goToCheckout"> 前往結帳 </MainButton>
-          <SubButton @click="goToFeatured">繼續購物</SubButton>
+      <div v-show="!isCartEmpty" class="min-w-[400px] md:max-w-240 lg:min-w-64">
+        <div class="p-5 bg-white rounded-md flex flex-col justify-between">
+          <div>
+            <h1>結算資訊</h1>
+            <p class="text-gray-400 text-sm mb-5">請選擇要結帳的行程</p>
+          </div>
+          <div>
+            <div class="space-y-3 mb-6">
+              <div class="flex justify-between text-gray-700">
+                <span>人數</span>
+                <span>{{ selectedTour ? selectedTour.persons : 0 }}</span>
+              </div>
+              <div class="flex justify-between text-gray-700">
+                <span>金額 </span>
+                <span>NT$ {{ selectedTour ? selectedTour.price : 0 }} </span>
+              </div>
+            </div>
+            <div class="border-t border-gray-300 pt-3 pb-3">
+              <div class="flex justify-between text-gray-700">
+                <span>小記</span>
+                <span>NT$ {{ totalPrice }}</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col gap-5">
+            <MainButton @click="goToCheckout"> 前往結帳 </MainButton>
+            <SubButton @click="goToFeatured">繼續購物</SubButton>
+          </div>
         </div>
       </div>
     </div>
