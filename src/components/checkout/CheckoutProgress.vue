e7ca2a3c598934e7f705f85d7c7b5ad7228a6bb5
chyt93620 <chyt93620@gmail.com>
2025-12-22 16:32:37 +0800

    style:切進度條-小-中

--- src/components/checkout/CheckoutProgress.vue
+++ src/components/checkout/CheckoutProgress.vue
@@ -12,16 +12,16 @@ const steps = ['確認商品', '填寫資料', '確認資料', '付款', '完成
 </script>
 
 <template>
-  <section class="flex justify-center mt-5 min-w-[400px]">
-    <ul class="flex gap-3 ">
-      <li v-for="(step, index) in steps" :key="step" class="flex">
+  <section class="flex justify-center mt-5">
+    <ul class="flex gap-3 sm:gap-5">
+      <li v-for="(step, index) in steps" :key="step" class="flex gap-1 sm:gap-2">
         <p
-          class="rounded-full w-5 h-5 text-center content-center text-sm"
+          class="rounded-full w-4 h-4 text-center content-center text-[12px] sm:w-8 sm:h-8 sm:text-[15px]"
           :class="index + 1 <= currentStep ? 'bg-blue-600 text-white' : 'bg-gray-300 text-gray-800'"
         >
           {{ index + 1 }}
         </p>
-        <p class="text-center text-sm">{{ step }}</p>
+        <p class="text-center text-[10px] sm:text-[15px]">{{ step }}</p>
       </li>
     </ul>
   </section>
