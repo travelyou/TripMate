3b9785712d2b99e798e6b55a59fa4d61e01cb18b
yuantai <yuntailee0615@gmail.com>
2026-01-08 20:55:50 +0800

    test: 資料庫連接測試

--- backend/server.js
+++ backend/server.js
@@ -51,15 +51,19 @@ app.get('/api/test-db', async (req, res) => {
 
 // 使用貼文路由
 app.use('/api/posts', postsRouter);
+app.use('/posts', postsRouter);
 
 // 使用留言路由
 app.use('/api', commentsRouter);
+app.use('/', commentsRouter);
 
 // 使用按讚路由
 app.use('/api', likesRouter);
+app.use('/', likesRouter);
 
 // 使用用戶路由
 app.use('/api/users', usersRouter);
+app.use('/users', usersRouter);
 
 app.listen(PORT, HOST, () => {
   console.log(`伺服器連接成功在 http://127.0.0.1:${PORT}`);
