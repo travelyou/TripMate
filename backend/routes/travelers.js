a7b34dcaf7b32edd293f2ec8a47d09c1d497b7ee
redpiigpig <redpiigpig@gmail.com>
2026-01-19 17:14:57 +0800

    fix:修補找旅伴日程問題

--- backend/routes/travelers.js
+++ backend/routes/travelers.js
@@ -119,17 +119,6 @@ router.get('/', async (req, res) => {
               minute: '2-digit',
               hour12: false,
             })
-      const timeStr =
-        diffSeconds < 600
-          ? '剛剛'
-          : created.toLocaleString('zh-TW', {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-              hour12: false,
-            })
 
       return {
         id: row.id,
