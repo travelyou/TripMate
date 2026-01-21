87071366ebd3a4ae7c15ec963e8ccf4123015317
yuantai <yuntailee0615@gmail.com>
2026-01-21 10:50:12 +0800

    移除重複的 src 和根目錄配置檔案

--- src/components/checkout/MainButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <button
-    class="px-8 py-2 text-center bg-primary text-white rounded-md hover:bg-primary/80 disabled:bg-gray-600"
-  >
-    <slot> 按鈕文字 </slot>
-  </button>
-
-</template>
